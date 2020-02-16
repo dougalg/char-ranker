86223c16af5208f1a06e90205dec9c62b0ce6a9f
dougalg <dougalg@gmail.com>
2020-02-16 08:50:54 +0700

    Filter characters > 1 char

--- app/app.py
+++ app/app.py
@@ -1,19 +1,43 @@
 import os
+import unicodedata
+import regex
 
-from flask import Flask
+from flask import Flask, request, jsonify, abort, make_response
 from redis import Redis
-
 
 app = Flask(__name__)
 redis = Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'])
 
 
-@app.route('/api')
-def hello2():
-	redis.incr('hits')
-	total_hits = redis.get('hits').decode()
-	return f'Hello from Redis! I have been seen {total_hits} times.'
+CHAR_SET = 'CHAR SET'
 
+@app.route('/api/char/<char>', methods = ['PUT', 'DELETE'])
+def upDown(char):
+	assert_valid_char(char)
+	inc = -1 if request.method == 'DELETE' else 1
+	val = redis.zincrby(CHAR_SET, inc, char)
+	return char_response(char, val)
+
+@app.route('/api/char/<char>', methods = ['GET'])
+def retrieve(char):
+	assert_valid_char(char)
+	val = redis.zscore(CHAR_SET, char)
+	return char_response(char, val)
+
+def char_response(char, val):
+	val = 0 if val == None else val
+	result = {
+		'char': char,
+		'val': val
+	}
+	return jsonify(result)
+
+def assert_valid_char(char):
+	if get_unicode_char_length(char) > 1:
+		abort(make_response(jsonify({ "error_message": "'{}' is not a valid char. It is too long: {}".format(char, get_unicode_char_length(char)) }), 404))
+
+def get_unicode_char_length(char):
+	return len(regex.findall(r'\X', char))
 
 if __name__ == "__main__":
 	bind_port = int(os.environ['BIND_PORT'])
